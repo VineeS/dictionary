6ad5accc236a21655c9ebb11ba59375ada5615c0
Vinee Shukla <vinee@Vinees-MBP.fios-router.home>
2019-11-11 15:41:40 -0500

    dictionary code initial commit

---
+++ buitDictionary.py
@@ -0,0 +1,24 @@
+import json
+from difflib import get_close_matches
+
+def dictionary(word):
+    data = json.load(open("/Users/vinee/Downloads/data.json"))
+    word = word.lower()
+    if word in data:
+        print(f'{word} :' , data[word])
+    elif word not in data:
+        final = get_close_matches(word,data)[0]
+        close_enough = input(f'do you mean "{final}" ? : ')
+        if close_enough == "yes":
+            print(data[final])
+        elif close_enough == "no":
+            print("The word does not exist please double check it ")
+        else: 
+            print("We did not Understand your input")
+    else:
+        print("The word is not in the dictionary")
+
+word = input("Enter word : ")
+dictionary(word)
+
+
