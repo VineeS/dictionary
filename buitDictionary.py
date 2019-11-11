9174c3fbb6191c155bef043d06ba6e834c81e532
Vinee Shukla <vinee@Vinees-MBP.fios-router.home>
2019-11-11 16:58:44 -0500

    commit 4

--- buitDictionary.py
+++ buitDictionary.py
@@ -1,11 +1,16 @@
 import json
 from difflib import get_close_matches
 
+data = json.load(open("/Users/vinee/Downloads/data.json"))
+
 def dictionary(word):
-    data = json.load(open("/Users/vinee/Downloads/data.json"))
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         final = get_close_matches(word,data)[0]
         close_enough = input(f'do you mean "{final}" ? : ')
@@ -21,7 +26,9 @@ def dictionary(word):
 word = input("Enter word : ")
 output = dictionary(word)
 
-for i in output:
-    print(i)
+if type(output) == list:
+    for i in output:
+        print(i)
+   
 
 
